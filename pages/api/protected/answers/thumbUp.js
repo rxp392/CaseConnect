124feeb59155428ea9a5157093af6eefa7fe5d94
stanfish06 <93099613+stanfish06@users.noreply.github.com>
2022-04-09 19:32:11 -0400

    Add files via upload

---
+++ pages/api/protected/answers/thumbUp.js
@@ -0,0 +1,24 @@
+import prisma from "lib/prisma";
+
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(404).json({ error: "Method not allowed" });
+  }
+  try {
+    const {id} = req.body;
+    await prisma.answer.update({
+      where: {
+        id: id, 
+      },
+      data: {
+        numThumbsUp: {increment: 1}
+      },
+    });
+
+    return res.status(200).json();
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error });
+  }
+}
