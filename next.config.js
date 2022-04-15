6cb7a724a211e59dbf7b88e4f3283bbc6c6e9420
Josh Levy <jml312@case.edu>
2022-04-15 11:32:25 -0400

    preact

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom": "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+      });
+    }
+    return config;
+  },
   async rewrites() {
     return [
       {
