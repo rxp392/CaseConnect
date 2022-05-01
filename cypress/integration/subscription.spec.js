3479c79b76b97080b76b9327c385f3830cd3e635
Josh Levy <59902856+jml312@users.noreply.github.com>
2022-05-01 13:24:54 -0400

    Update subscription.spec.js

--- cypress/integration/subscription.spec.js
+++ cypress/integration/subscription.spec.js
@@ -1,36 +1,30 @@
 /// <reference types="Cypress" />
-describe("SubscriptionPage", () => {
-    it("should return the subscription page",() => {
-        cy.login({isFirstLogin: false});
-        cy.visit("/subscription");
-    } )
-    it("check for current plan",() => {
-        cy.get(".css-1gd1h4t").contains("Basic Plan"); 
-        cy.get(".css-pq4kmx").should("exist");
-        cy.get(".css-1yrxwy4").should("exist");
-    } )
-    it("check for Upgrade button",() => {
-        cy.get(".css-1bssxr9").contains("Upgrade Now"); 
-    } )
-    // it("check for Premium", () => {
-    //     cy.login({ isFirstLogin: false });
-    //     cy.visit("/subscription");
-    //     cy.request({
-    //       method: "POST",
-    //       url: "/api/test/change-subscription",
-    //       body: { caseId: "test", subscription: "Premium" },
-    //     })
-    //       .then(() => cy.reload())
-    //       .then(() =>
-    //         cy.request({
-    //           method: "POST",
-    //           url: "/api/test/change-subscription",
-    //           body: { caseId: "test", subscription: "Basic" },
-    //         })
-    //       );
-    //     //cy.get(".css-1gd1h4t").contains("Premium Plan");
-    //   });
-      
+describe("Subscription Page", () => {
+  beforeEach(() => {
+    cy.login({ isFirstLogin: false });
+    cy.visit("/subscription");
+  });
+  afterEach(() => {
+    cy.logout();
+  });
 
+  it("should show a basic subscription correctly", () => {
+    cy.get("button.css-1bssxr9").should("exist");
+  });
 
+  it("should show a premium subscription correctly", () => {
+    cy.request("POST", "/api/test/change-subscription", {
+      caseId: "test",
+      subscription: "Premium",
+    });
+    cy.reload();
+    cy.get("h2.css-121pujs").should(
+      "contain",
+      "Congratulations! You're a Premium user"
+    );
+    cy.get("p.css-hvlzr0").should(
+      "contain",
+      "You can now ask and view unlimited questions"
+    );
+  });
 });
